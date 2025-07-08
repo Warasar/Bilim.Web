cb7cfa664db8e3815b4bc38e58aca6b44e6a63d3
Pavel <starostin.pavelwtf13@gmail.com>
2025-07-08 17:59:23 +0900

    added fake data 3/15

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,6 +28,7 @@ type Props = {
 export default function Header({ data, whiteBg }: Props) {
   const [isScrolled, setIsScrolled] = useState<boolean>(whiteBg ? true : false);
   const [showPopover, setShowPopover] = useState<boolean>(false);
+  const [activeSection, setActiveSection] = useState<string | null>(null);
 
   const headerClassName = !isScrolled ? "header" : "header-scrolled";
   const location = useLocation();
@@ -37,6 +38,18 @@ export default function Header({ data, whiteBg }: Props) {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       setIsScrolled(scrollPosition > 10);
+
+      document.querySelectorAll(`[id*="vuz_"]`).forEach((element: any) => {
+        if (element) {
+          const offsetTop = element.offsetTop;
+          const offsetHeight = element.offsetHeight;
+          if (scrollPosition + 100 >= offsetTop && scrollPosition + 100 < offsetTop + offsetHeight) {
+            setActiveSection(element.id);
+          } else if (scrollPosition < 100) {
+            setActiveSection(null);
+          }
+        }
+      });
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -48,6 +61,23 @@ export default function Header({ data, whiteBg }: Props) {
 
   const clickUser = () => {
     setShowPopover(!showPopover);
+  };
+
+  const clickScrollIntoDiv = (id: any) => {
+    if (id === "vuz") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    const element = document.getElementById(`vuz_${id}`);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop - 20,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
@@ -70,11 +100,27 @@ export default function Header({ data, whiteBg }: Props) {
         <div className={`${headerClassName}-flex`}>
           {data.buttons.map((item: any) => {
             if (item.scroll) {
+              if (!activeSection && item.link === "vuz") {
+                return (
+                  <NavLink
+                    to={`/${item.link}`}
+                    className={`${headerClassName}-flex-item${location.pathname === `/${item.link}` ? "-active" : ""}`}
+                    onClick={() =>
+                      window.scrollTo({
+                        top: 0,
+                        behavior: "smooth", // для плавной прокрутки
+                      })
+                    }
+                  >
+                    {item.name}
+                  </NavLink>
+                );
+              }
+
               return (
                 <div
-                  className={`${headerClassName}-flex-item${
-                    location.pathname === `/${item.link}` ? "-active" : ""
-                  }`}
+                  className={`${headerClassName}-flex-item${activeSection === `vuz_${item.link}` ? "-active" : ""}`}
+                  onClick={() => clickScrollIntoDiv(item.link)}
                 >
                   {item.name}
                 </div>
@@ -84,9 +130,7 @@ export default function Header({ data, whiteBg }: Props) {
             return (
               <NavLink
                 to={`/${item.link}`}
-                className={`${headerClassName}-flex-item${
-                  location.pathname === `/${item.link}` ? "-active" : ""
-                }`}
+                className={`${headerClassName}-flex-item${location.pathname === `/${item.link}` ? "-active" : ""}`}
                 onClick={() =>
                   window.scrollTo({
                     top: 0,
@@ -102,31 +146,19 @@ export default function Header({ data, whiteBg }: Props) {
 
         {/* связаться и пользователь */}
         <div className={`${headerClassName}-flex`}>
-          <a
-            className={`${headerClassName}-button`}
-            href={data.phoneLink}
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a className={`${headerClassName}-button`} href={data.phoneLink} target="_blank" rel="noreferrer">
             <div className={`${headerClassName}-button-mail`} />
             Связаться
           </a>
           <div className={`header-popoverParent`} ref={popoverRef}>
-            <div
-              className={`${headerClassName}-user`}
-              onClick={() => clickUser()}
-            />
+            <div className={`${headerClassName}-user`} onClick={() => clickUser()} />
             <div className={`header-popover${showPopover ? "-active" : ""}`}>
               <div className={`header-popover-container`}>
                 <div className={`header-popover-user`}>
                   <div className={`header-popover-user-icon`} />
                   <div className="header-popover-user-item">
-                    <div className="header-popover-user-item-name">
-                      {data.user.name}
-                    </div>
-                    <div className="header-popover-user-item-mail">
-                      {data.user.mail}
-                    </div>
+                    <div className="header-popover-user-item-name">{data.user.name}</div>
+                    <div className="header-popover-user-item-mail">{data.user.mail}</div>
                   </div>
                 </div>
 
@@ -147,9 +179,7 @@ export default function Header({ data, whiteBg }: Props) {
                       <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
                       <circle cx="12" cy="7" r="4"></circle>
                     </svg>
-                    <div className="header-popover-items-item-text">
-                      Мой профиль
-                    </div>
+                    <div className="header-popover-items-item-text">Мой профиль</div>
                   </div>
                   <div className="header-popover-items-item">
                     <svg
@@ -167,9 +197,7 @@ export default function Header({ data, whiteBg }: Props) {
                       <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"></path>
                       <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"></path>
                     </svg>
-                    <div className="header-popover-items-item-text">
-                      Уведомления
-                    </div>
+                    <div className="header-popover-items-item-text">Уведомления</div>
                   </div>
                   <div className="header-popover-items-item">
                     <svg
@@ -187,9 +215,7 @@ export default function Header({ data, whiteBg }: Props) {
                       <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path>
                       <circle cx="12" cy="12" r="3"></circle>
                     </svg>
-                    <div className="header-popover-items-item-text">
-                      Настройки
-                    </div>
+                    <div className="header-popover-items-item-text">Настройки</div>
                   </div>
                 </div>
 
@@ -197,10 +223,7 @@ export default function Header({ data, whiteBg }: Props) {
                   <div className={`header-popover-items-title`}>Группы</div>
                   {data.groups.map((item: any) => {
                     return (
-                      <NavLink
-                        className={`header-popover-items-text`}
-                        to={`/${item.link}`}
-                      >
+                      <NavLink className={`header-popover-items-text`} to={`/${item.link}`}>
                         {item.name}
                       </NavLink>
                     );
--- src/components/University/University.tsx
+++ src/components/University/University.tsx
@@ -9,6 +9,7 @@ type Props = {
 
 export default function University({ findData, className }: Props) {
   const [data, setData] = useState<any>(null);
+  const [activeTab, setActiveTab] = useState<number>(0);
 
   useEffect(() => {
     if (findData) {
@@ -19,8 +20,8 @@ export default function University({ findData, className }: Props) {
   const clickFakultet = (id: number) => {
     const newData: any = _.cloneDeep(data);
 
-    newData.fakultet.data.find((f: any) => f.id === id).opened =
-      !newData.fakultet.data.find((f: any) => f.id === id).opened;
+    newData.fakultet.data.find((f: any) => f.id === id).opened = !newData.fakultet.data.find((f: any) => f.id === id)
+      .opened;
 
     setData(newData);
   };
@@ -28,118 +29,214 @@ export default function University({ findData, className }: Props) {
   const clickChild = (id: number, child_id: number) => {
     const newData: any = _.cloneDeep(data);
 
-    newData.fakultet.data
-      .find((f: any) => f.id === id)
-      .items.find((f: any) => f.id === child_id).opened = !newData.fakultet.data
-      .find((f: any) => f.id === id)
-      .items.find((f: any) => f.id === child_id).opened;
+    newData.fakultet.data.find((f: any) => f.id === id).items.find((f: any) => f.id === child_id).opened =
+      !newData.fakultet.data.find((f: any) => f.id === id).items.find((f: any) => f.id === child_id).opened;
 
     setData(newData);
   };
 
-  return (
-    <div className={className}>
-      <div className={`${className}-main`}>
-        <div className={`${className}-header`}>
-          <div className={`${className}-header-left`}>
-            <div className={`${className}-header-left-title`}>{data?.name}</div>
-            <div className={`${className}-header-left-text`}>
-              {data?.content}
-            </div>
-          </div>
-
-          <iframe
-            src={data?.iframe}
-            className={`${className}-header-right`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </div>
-
-        <div className={`${className}-fakultet`}>
-          <div className={`${className}-fakultet-header`}>
-            <div className={`${className}-fakultet-header-title`}>
-              {data?.fakultet?.title}
-            </div>
-          </div>
-
-          <div className={`${className}-fakultet-content`}>
-            {data?.fakultet?.data?.map((item: any) => {
-              return (
-                <div
-                  className={`${className}-fakultet-item`}
-                  key={`${className}-fakultet-item_${item.id}`}
-                >
-                  <div className={`${className}-fakultet-item-grid`}>
-                    <div className={`${className}-fakultet-item-name`}>
-                      {item.name}
-                    </div>
-                    <div
-                      className={`${className}-fakultet-item-arrow${
-                        item.opened ? "-active" : ""
-                      }`}
-                      onClick={() => clickFakultet(item.id)}
-                    />
-                  </div>
-
-                  <div
-                    className={`${className}-fakultet-items${
-                      item.opened ? "-active" : ""
-                    }`}
-                  >
-                    {item.items.map((child: any) => {
-                      return (
-                        <div className={`${className}-fakultet-child`}>
-                          <div />
-                          <div className={`${className}-fakultet-child-header`}>
-                            <div
-                              className={`${className}-fakultet-child-header-grid`}
-                            >
-                              <div
-                                className={`${className}-fakultet-child-header-text`}
-                              >
-                                {child.name}
-                              </div>
-                              <div
-                                className={`${className}-fakultet-child-header-plus${
-                                  child.opened ? "-active" : ""
-                                }`}
-                                onClick={() => clickChild(item.id, child.id)}
-                              />
-                            </div>
-
-                            <div
-                              className={`${className}-fakultet-child-content${
-                                child.opened ? "-active" : ""
-                              }`}
-                            >
-                              <div
-                                className={`${className}-fakultet-child-content-text`}
-                              >
-                                {child.text}
-                              </div>
-                              <div
-                                className={`${className}-fakultet-child-content-price`}
-                              >
-                                СТОИМОСТЬ
-                              </div>
-                              <div
-                                className={`${className}-fakultet-child-content-text`}
-                              >
-                                {child.price}
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+  const renderTabItem = (activeItem: any) => {
+    return activeItem ? (
+      <div className={`${className}-steps-item`}>
+        <div className={`${className}-steps-item-title`}>{activeItem.title}</div>
+        <div className={`${className}-steps-item-subtitle`}>{activeItem.subtitle}</div>
+        <div className={`${className}-steps-item-grid`}>
+          {activeItem.items.map((item: any) => {
+            return renderBlockItem(item);
+          })}
         </div>
       </div>
+    ) : null;
+  };
+
+  const renderBlockItem = (item: any, cmd?: "docs") => {
+    return (
+      <div className={`${className}-steps-item-block`} key={`${className}-steps-item-block_${item.id}`}>
+        {cmd === "docs" ? (
+          <div className={`${className}-docs-circle`}>{item.id + 1}</div>
+        ) : (
+          <div className={`${className}-steps-item-block-icon`} />
+        )}
+        <div className={`${className}-steps-item-block-texts`}>
+          {item.title?.length ? <div className={`${className}-steps-item-block-title`}>{item.title}</div> : null}
+          <div className={`${className}-steps-item-block-content`}>{item.content}</div>
+        </div>
+      </div>
+    );
+  };
+
+  const getGrid = (length: number) => {
+    const style: React.CSSProperties = {};
+
+    style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+
+    return style;
+  };
+
+  const getLine = (length: number) => {
+    const style: React.CSSProperties = {};
+
+    if (length === 2) {
+      style.left = `25%`;
+      style.right = `25%`;
+    } else if (length === 3) {
+      style.left = `15%`;
+      style.right = `15%`;
+    } else if (length === 4) {
+      style.left = `12%`;
+      style.right = `12%`;
+    }
+
+    return style;
+  };
+
+  return (
+    <div className={className}>
+      {data ? (
+        <div className={`${className}-main`}>
+          <div className={`${className}-header`}>
+            <div className={`${className}-header-left`}>
+              <div className={`${className}-header-left-title`}>{data?.name}</div>
+              <div className={`${className}-header-left-text`}>{data?.content}</div>
+            </div>
+
+            <iframe
+              src={data?.iframe}
+              className={`${className}-header-right`}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+
+          {data.fakultet ? (
+            <div className={`${className}-fakultet`} id={"vuz_fakultet"}>
+              <div className={`${className}-fakultet-header`}>
+                <div className={`${className}-fakultet-header-title`}>{data?.fakultet?.title}</div>
+              </div>
+
+              <div className={`${className}-fakultet-content`}>
+                {data.fakultet.data.map((item: any) => {
+                  return (
+                    <div className={`${className}-fakultet-item`} key={`${className}-fakultet-item_${item.id}`}>
+                      <div className={`${className}-fakultet-item-grid`}>
+                        <div className={`${className}-fakultet-item-name`}>{item.name}</div>
+                        <div
+                          className={`${className}-fakultet-item-arrow${item.opened ? "-active" : ""}`}
+                          onClick={() => clickFakultet(item.id)}
+                        />
+                      </div>
+
+                      <div className={`${className}-fakultet-items${item.opened ? "-active" : ""}`}>
+                        {item.items.map((child: any) => {
+                          return (
+                            <div
+                              className={`${className}-fakultet-child`}
+                              key={`${className}-fakultet-child_${item.id}_${child.id}`}
+                            >
+                              <div />
+                              <div className={`${className}-fakultet-child-header`}>
+                                <div className={`${className}-fakultet-child-header-grid`}>
+                                  <div className={`${className}-fakultet-child-header-text`}>{child.name}</div>
+                                  <div
+                                    className={`${className}-fakultet-child-header-plus${
+                                      child.opened ? "-active" : ""
+                                    }`}
+                                    onClick={() => clickChild(item.id, child.id)}
+                                  />
+                                </div>
+
+                                <div className={`${className}-fakultet-child-content${child.opened ? "-active" : ""}`}>
+                                  <div className={`${className}-fakultet-child-content-text`}>{child.text}</div>
+                                  <div className={`${className}-fakultet-child-content-price`}>СТОИМОСТЬ</div>
+                                  <div className={`${className}-fakultet-child-content-text`}>{child.price}</div>
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          ) : null}
+
+          {data.steps ? (
+            <div className={`${className}-steps`} id={"vuz_steps"}>
+              <div className={`${className}-steps-title`}>{data.steps.title}</div>
+              <div className={`${className}-steps-content`}>
+                <div className={`${className}-steps-tabs`} style={getGrid(data.steps.tabs.length)}>
+                  <div className={`${className}-steps-tab-line`} style={getLine(data.steps.tabs.length)} />
+                  {data.steps.tabs.map((item: any, index: number) => {
+                    return (
+                      <div className={`${className}-steps-tab`} key={`${className}-steps-tab_${item.id}`}>
+                        <div className={`${className}-steps-tab-center`}>
+                          <div
+                            className={`${className}-steps-tab-circle${
+                              activeTab === index ? ` ${className}-steps-tab-circle-active` : ""
+                            }`}
+                            onClick={() => setActiveTab(index)}
+                          >
+                            <div className={`${className}-steps-tab-circle-number`}>{index + 1}</div>
+                          </div>
+                        </div>
+
+                        <div className={`${className}-steps-tab-title`}>{item.title}</div>
+                        <div className={`${className}-steps-tab-subtitle`}>{item.subtitle}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+
+                {renderTabItem(data.steps.tabs.find((f: any) => f.id === activeTab))}
+              </div>
+            </div>
+          ) : null}
+
+          {data.dormitory ? (
+            <div className={`${className}-dormitory`} id={"vuz_dormitory"}>
+              <div className={`${className}-dormitory-title`}>
+                {data.dormitory.title}
+                {data.dormitory.subtitle?.length ? (
+                  <div className={`${className}-dormitory-subtitle`}>{data.dormitory.subtitle}</div>
+                ) : null}
+              </div>
+              <div className={`${className}-dormitory-content`}>
+                {data.dormitory.text ? (
+                  <div className={`${className}-dormitory-text`}>{data.dormitory.text}</div>
+                ) : null}
+                {data.dormitory.iframe ? (
+                  <iframe
+                    src={data.dormitory.iframe}
+                    className={`${className}-dormitory-iframe`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                ) : null}
+                <div className={`${className}-dormitory-items`}>
+                  {data.dormitory.items.map((item: any) => {
+                    return renderBlockItem(item);
+                  })}
+                </div>
+              </div>
+            </div>
+          ) : null}
+
+          {data.docs ? (
+            <div className={`${className}-docs`} id={"vuz_docs"}>
+              <div className={`${className}-docs-title`}>{data.docs.title}</div>
+              <div className={`${className}-docs-content`}>
+                <div className={`${className}-docs-items`}>
+                  {data.docs.items.map((item: any) => {
+                    return renderBlockItem(item, "docs");
+                  })}
+                </div>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      ) : null}
     </div>
   );
 }
