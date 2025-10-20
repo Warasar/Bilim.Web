147ef2e0b1ca07896d8d323657466a55ed56969a
Pavel <starostin.pavelwtf13@gmail.com>
2025-10-20 17:17:20 +0900

    changes

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,113 +1,52 @@
 import React, { useEffect, useState } from "react";
 import "./profile.scss";
 import Loader from "../../modules/YaKIT.WEB.KIT/components/Loader/Loader";
-import Modal from "../../modules/YaKIT.WEB.KIT/components/Modal/Modal";
-import { requestGet, requestPost } from "../../actions/actions";
-import { DatePickerProps, message } from "antd";
-import Input from "../../modules/YaKIT.WEB.KIT/components/Input/Input";
-import _ from "lodash";
-import Select from "../../modules/YaKIT.WEB.KIT/components/Select/Select";
-import { DatePicker, ConfigProvider } from "antd";
+import { requestGet } from "../../actions/actions";
 import dayjs from "dayjs";
-import locale from "antd/locale/ru_RU";
 import HeaderContainer from "../Header/HeaderContainer";
+import { Image } from "antd";
+import userIcon from "../../assets/icons/default/user.svg";
 
 dayjs.locale("ru");
-const dateFormat = "DD MMMM YYYY"; // "7 августа 2000"
 
 export default function Profile() {
   const [loader, setLoader] = useState<boolean>(true);
-  const [showTermsModal, setShowTermsModal] = useState<boolean>(false);
-  const [terms, setTerms] = useState<any>(null);
   const [data, setData] = useState<any>(null);
-  const [currentUser, setCurrentUser] = useState<any>(null);
-
-  const customFormatDate: DatePickerProps["format"] = (value) => {
-    return dayjs(value).format(dateFormat);
-  };
+  const [sider, setSider] = useState<any>([
+    {
+      code: "0",
+      name: "Профиль",
+      icon: "profile",
+      active: true,
+    },
+    {
+      code: "1",
+      name: "Документы",
+      icon: "docs",
+      active: false,
+    },
+    {
+      code: "2",
+      name: "Уведомления",
+      icon: "notification",
+      active: false,
+    },
+    {
+      code: "3",
+      name: "Домашние задания",
+      icon: "homework",
+      active: false,
+    },
+  ]);
 
   // первоначалка
   useEffect(() => {
     getData();
   }, []);
   const getData = async () => {
-    const currentUser = await requestGet(`currentUser`);
-
-    setCurrentUser(currentUser);
-    setLoader(false);
-  };
-
-  // принимает пользовательское соглашение
-  const acceptTerms = async () => {
-    setLoader(true);
-
-    const accept = await requestPost(`register/acceptTerms`, {});
-
-    if (accept) {
-      message.success(accept);
-      setShowTermsModal(false);
-    }
-
-    setLoader(false);
-  };
-
-  // меняет input
-  const changedString = (code: string, e: any) => {
-    const newData: any = _.cloneDeep(data);
-
-    newData.data.find((f: any) => f.questionCode === code).value = e.target.value;
+    const newData: any = await requestGet(`container/user`);
 
     setData(newData);
-  };
-
-  // меняет object
-  const changedObject = (code: string, e: any) => {
-    const newData: any = _.cloneDeep(data);
-
-    newData.data.find((f: any) => f.questionCode === code).value = e;
-
-    setData(newData);
-  };
-
-  // меняет date
-  const changedDate = (code: string, e: any) => {
-    const newData: any = _.cloneDeep(data);
-
-    newData.data.find((f: any) => f.questionCode === code).value = e;
-
-    setData(newData);
-  };
-
-  const sendSurvey = async () => {
-    setLoader(true);
-
-    const sendObj: any = {};
-
-    data.data.forEach((item: any) => {
-      if (item.value) {
-        if (item.answerType === "date") {
-          sendObj[item.questionCode] = dayjs(item.value).format("YYYY-MM-DD");
-        } else if (item.answerType === "object") {
-          sendObj[item.questionCode] = item.value?.code;
-        } else {
-          sendObj[item.questionCode] = item.value;
-        }
-      }
-    });
-
-    const sendAnswer = await requestPost(`register/passSurvey`, sendObj);
-
-    if (sendAnswer) {
-      message.success("Успешно сохранено!");
-
-      setTimeout(() => {
-        const win: Window = window;
-        win.location = `${window.location.origin}`;
-      }, 2000);
-    } else {
-      message.error("Произошла ошибка, пожайлуста попробуйте позже или обратитесь к администратору");
-    }
-
     setLoader(false);
   };
 
@@ -117,39 +56,50 @@ export default function Profile() {
       <HeaderContainer />
 
       <div className="profile">
-        <div className="profile-container">
-          <div className="profile-title">Профиль</div>
+        {data ? (
+          <div className="profile-container">
+            <div className="profile-block">
+              <div className="profile-sider">
+                {sider.map((item: any) => {
+                  return (
+                    <div className={"profile-sider-item" + (item.active ? "-active" : "")}>
+                      <div className={`profile-sider-item-icon-${item.icon}`} />
+                      <div className={"profile-sider-item-text" + (item.active ? "-active" : "")}>{item.name}</div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="profile-user">
+                <Image
+                  className="profile-user-avatar"
+                  // src={`${item.urlImg}`}
+                  style={{
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                    overflow: "hidden",
+                    objectPosition: "top",
+                  }}
+                  preview={{
+                    mask: <>Просмотр</>,
+                  }}
+                  fallback={userIcon}
+                />
 
-          <div className="profile-user">
-            <div className="profile-user-avatar">avatar aang</div>
+                <div className="profile-user-items">
+                  <div className="profile-user-item">
+                    <div className="profile-user-item-text profile-user-item-text-bold">ФИО:</div>
+                    <div className="profile-user-item-text">{data.fullName}</div>
+                  </div>
 
-            <div className="profile-user-items">
-              <div className="profile-user-item">
-                <div className="profile-user-item-text">Имя:</div>
+                  <div className="profile-user-item">
+                    <div className="profile-user-item-text profile-user-item-text-bold">Email:</div>
+                    <div className="profile-user-item-text">{data.mail}</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        {/* пользовательское соглашение */}
-        <Modal open={showTermsModal} onCancel={() => {}}>
-          <div className="survey-modal">
-            <div className="survey-modal-header">
-              <div className="survey-modal-header-text">Условия пользования</div>
-            </div>
-            <div className="survey-modal-acceptTerms">
-              <div className="survey-modal-acceptTerms-text">{terms?.text1}</div>
-              <div className="survey-modal-acceptTerms-text">{terms?.text2}</div>
-              <div className="survey-modal-acceptTerms-text">{terms?.text3}</div>
-            </div>
-            <div className="survey-modal-footer">
-              <div />
-              <div className="survey-modal-footer-button" onClick={() => acceptTerms()}>
-                Принять
-              </div>
-            </div>
-          </div>
-        </Modal>
+        ) : null}
       </div>
     </div>
   );
--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,2 +1,6 @@
 declare module "react-dynamic-virtual-scroll";
 declare module "react-typewriter-effect";
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
